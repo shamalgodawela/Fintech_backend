2ea8c942395c471ac1397b794e5ee5d020761189
Your Name <lenmicr250@gmail.com>
2025-03-29 00:10:40 +0530

    update

--- api/Models/Budget.js
+++ api/Models/Budget.js
@@ -1,22 +1,21 @@
-import mongoose from 'mongoose';
-
-const budgetSchema = new mongoose.Schema({
-  amount: { type: Number, required: true },
-  category: { type: String, required: true },
-  notes: { type: String, default: "" },
-  startDate: { type: Date, required: true },
-  responsiblePerson: {
-    type: String,
-    trim: true
-},
-phone:{
-  type: String,
-  required: true
-
-}
-
-
-});
-
-
+import mongoose from 'mongoose';
+const budgetSchema = new mongoose.Schema({
+  amount: { type: Number, required: true },
+  category: { type: String, required: true },
+  notes: { type: String, default: "" },
+  startDate: { type: Date, required: true },
+  responsiblePerson: {
+    type: String,
+    trim: true
+},
+phone:{
+  type: String,
+  required: true
+
+}
+
+
+});
+
+
 export default mongoose.model('Budget', budgetSchema);
